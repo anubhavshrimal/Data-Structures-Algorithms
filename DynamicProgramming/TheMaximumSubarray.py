46b461e748d7db0bfae9692aafbd088b1b62993d
anubhavshrimal <anubhavshrimal@gmail.com>
2016-10-20 01:07:07 +0530

    maximum sum subarray

---
+++ DynamicProgramming/TheMaximumSubarray.py
@@ -0,0 +1,26 @@
+
+
+def maxSubArray(a, size):
+    currMax = a[0]
+    maxSoFar = a[0]
+
+    for i in range(1, size):
+        currMax = max(a[i], currMax + a[i])
+        maxSoFar = max(currMax, maxSoFar)
+
+    return maxSoFar
+
+
+
+
+testcases = int(input())
+
+for t in range(testcases):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    tempList = list(filter(lambda x: x > 0, arr))
+    if len(tempList) != 0:
+        maximumSum = sum(tempList)
+    else:
+        maximumSum = max(arr)
+    print(maxSubArray(arr, len(arr)), maximumSum)
