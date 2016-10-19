62758ba504741532e787cfd1d7a63a25d0f1f6e2
anubhavshrimal <anubhavshrimal@gmail.com>
2016-10-19 10:15:23 +0530

    Fibonacci Modified in hackerRank

---
+++ HackerRank/DynamicProgramming/FibonacciModified.py
@@ -0,0 +1,14 @@
+
+t1, t2, n = input().split()
+t1 = int(t1)
+t2 = int(t2)
+n = int(n)
+
+arr = [0]*n
+arr[0] = t1
+arr[1] = t2
+for i in range(2, n):
+    arr[i] = arr[i-2] + arr[i-1] ** 2
+
+print(arr[-1])
+
