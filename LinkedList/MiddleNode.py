e51dd79174b54fd4482bd740bcd1a7aa1e24b5d0
anubhavshrimal <anubhavshrimal@gmail.com>
2016-12-30 22:03:21 +0530

    added find a middle node of a linked list

---
+++ LinkedList/MiddleNode.py
@@ -0,0 +1,65 @@
+# Python program to find middle node of a singly linked list
+
+
+# Node class
+class Node:
+
+    # Constructor to initialise data and next
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+
+    # Constructor to initialise head
+    def __init__(self):
+        self.head = None
+
+    # Function to reverse K nodes of linked list
+    def find_mid(self, head):
+        fast = head
+        slow = head
+
+        # Make fast run twice the speed of slow
+        # when fast is at the last of the list
+        # slow will be at the mid node
+        while fast is not None:
+            fast = fast.next
+            if fast is not None:
+                fast = fast.next
+                slow = slow.next
+
+        return slow
+
+    # Function to Insert data at the beginning of the linked list
+    def insert_at_beg(self, data):
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+
+    # Function to print the linked list
+    def print_data(self):
+        current = self.head
+        while current is not None:
+            print(current.data, '-> ', end='')
+            current = current.next
+        print('None')
+
+if __name__ == '__main__':
+    linked_list = SinglyLinkedList()
+    linked_list.insert_at_beg(9)
+    linked_list.insert_at_beg(8)
+    linked_list.insert_at_beg(7)
+    linked_list.insert_at_beg(6)
+    linked_list.insert_at_beg(5)
+    linked_list.insert_at_beg(4)
+    linked_list.insert_at_beg(3)
+    linked_list.insert_at_beg(2)
+    linked_list.insert_at_beg(1)
+    linked_list.print_data()
+    # call the find_mid function
+    mid = linked_list.find_mid(linked_list.head)
+    # print the middle node if not None
+    if mid is not None:
+        print(mid.data)
