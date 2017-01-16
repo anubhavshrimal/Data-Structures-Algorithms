1438c0915972f254d3a589ca064f54b6f024d5d6
anubhavshrimal <anubhavshrimal@gmail.com>
2017-01-16 22:10:00 +0530

    made a correction

--- Graph/breadthFirstTraversal.py
+++ Graph/breadthFirstTraversal.py
@@ -47,7 +47,6 @@ if __name__ == '__main__':
     graph.addEdge(0, 1)
     graph.addEdge(0, 2)
     graph.addEdge(1, 2)
-    graph.addEdge(2, 0)
     graph.addEdge(2, 3)
     graph.addEdge(3, 3)
     graph.addEdge(1, 4)
