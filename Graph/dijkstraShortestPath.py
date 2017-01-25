3ddb9be0b9c16c747fc3948866e884745d5519b5
anubhavshrimal <anubhavshrimal@gmail.com>
2017-01-25 22:00:00 +0530

    Dijkstra’s Shortest Path Algorithm

---
+++ Graph/dijkstraShortestPath.py
@@ -0,0 +1,87 @@
+# Given a graph and a source vertex in graph, find shortest paths from source to all vertices in the given graph
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, directed=False):
+        self.graph = defaultdict(list)
+        self.directed = directed
+
+    def addEdge(self, frm, to, weight):
+        self.graph[frm].append([to, weight])
+
+        if self.directed is False:
+            self.graph[to].append([frm, weight])
+        else:
+            self.graph[to] = self.graph[to]
+
+    def find_min(self, dist, visited):
+        minimum = float('inf')
+        index = -1
+        for v in self.graph.keys():
+            if visited[v] is False and dist[v] < minimum:
+                minimum = dist[v]
+                index = v
+
+        return index
+
+    def dikstra(self, src):
+        visited = {i: False for i in self.graph}
+        dist = {i: float('inf') for i in self.graph}
+        parent = {i: None for i in self.graph}
+
+        # set distance of src vertex from itself 0
+        dist[src] = 0
+
+        for i in range(len(self.graph)-1):
+            u = self.find_min(dist, visited)
+
+            visited[u] = True
+
+            for v, w in self.graph[u]:
+                if visited[v] is False and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+                    parent[v] = u
+
+        return parent, dist
+
+    def printPath(self, parent, v):
+        if parent[v] is None:
+            return
+        self.printPath(parent, parent[v])
+        print(v,end=' ')
+
+    def printSolution(self, dist, parent, src):
+        print('{}\t{}\t{}'.format('Vertex', 'Distance', 'Path'))
+
+        for i in self.graph.keys():
+            if i == src:
+                continue
+            print('{} -> {}\t\t{}\t\t{}'.format(src, i, dist[i], src), end=' ')
+            self.printPath(parent,i)
+            print()
+
+if __name__ == '__main__':
+    # make an undirected graph
+    graph = Graph()
+
+    graph.addEdge(0, 1, 4)
+    graph.addEdge(0, 7, 8)
+    graph.addEdge(1, 2, 8)
+    graph.addEdge(1, 7, 11)
+    graph.addEdge(7, 6, 1)
+    graph.addEdge(7, 8, 7)
+    graph.addEdge(6, 8, 6)
+    graph.addEdge(6, 5, 2)
+    graph.addEdge(8, 2, 2)
+    graph.addEdge(2, 3, 7)
+    graph.addEdge(2, 5, 4)
+    graph.addEdge(3, 4, 9)
+    graph.addEdge(3, 5, 14)
+    graph.addEdge(5, 4, 10)
+
+    parent, dist = graph.dikstra(0)
+
+    graph.printSolution(dist, parent, 0)
+
+
