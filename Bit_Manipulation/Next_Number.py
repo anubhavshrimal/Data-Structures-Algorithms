f36ba27fc561a141a7b647b1c8d2c51123b2d281
anubhavshrimal <anubhavshrimal@gmail.com>
2017-03-03 22:00:00 +0530

    added comments to Next_Number.py

--- Bit_Manipulation/Next_Number.py
+++ Bit_Manipulation/Next_Number.py
@@ -1,12 +1,23 @@
 # Find the next greater and next smaller number with same number of set bits
 
-
+# Ex. num = 6 bin = 110
 def next_greater(num):
     res = num
     if num != 0:
+        # Find the right most 1 position
+        # Ex. right_one = 2 bin = 10
         right_one = num & -num
+
+        # get the left pattern to merge
+        # Ex. left_pattern = 8 bin = 1000
         left_pattern = num + right_one
+
+        # get the right patten to merge
+        # Ex. right_pattern = 1 bin = 0001
         right_pattern = (num ^ left_pattern) >> (right_one + 1)
+
+        # OR both the patterns
+        # Ex. res = 0 bin = 1001
         res = left_pattern | right_pattern
 
     return res
