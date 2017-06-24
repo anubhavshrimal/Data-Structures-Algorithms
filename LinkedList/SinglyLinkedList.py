a08e047505dac396f2d7efe0b9aa55a5e0ae7b6c
anubhavshrimal <anubhavshrimal@gmail.com>
2017-06-24 15:20:24 +0530

    Updated SinglyLinkedList Code

--- LinkedList/SinglyLinkedList.py
+++ LinkedList/SinglyLinkedList.py
@@ -1,56 +1,56 @@
-#!/usr/bin/python3
-
-
+# class of a node
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
-    def get_data(self):
-        return self.data
 
-    def get_next(self):
-        return self.next
-
-    def set_data(self, newdata):
-        self.data = newdata
-
-    def set_next(self, node):
-        self.next = node
-
-
+# class of the signly linked list
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
 
+    # function to calculate size of the list
     def size(self):
+        # initialize temporary variable and size to zero
         current = self.head
         size = 0
 
+        # count until current does not reach the end of the list i.e. NULL or None
         while current is not None:
             size += 1
-            current = current.get_next()
+            current = current.next
         return size
 
+    # function to insert at the end of the list
     def insert_at_end(self, data):
+        # Create the new node
         node = Node(data)
+
+        # If the list is empty
         if self.head is None:
             self.head = node
         else:
             current = self.head
 
-            while current.get_next() is not None:
-                current = current.get_next()
+            # Otherwise move to the last node of the list
+            while current.next is not None:
+                current = current.next
 
-            current.set_next(node)
-        self.print_data()
+            # Point the last node of the list to the new node
+            # So that the new node gets added to the end of the list
+            current.next = node
 
+    # function to insert at the beginning of the list
     def insert_at_beg(self, data):
         node = Node(data)
-        node.set_next(self.head)
-        self.head = node
-        self.print_data()
+        # Next pointer of the new node points to the head
+        node.next = self.head
 
+        # Updated the head as new node
+        self.head = node
+
+    # function to delete from the end of the list
     def delete_from_end(self):
         current = self.head
         previous = None
@@ -58,43 +58,62 @@ class SinglyLinkedList:
         if current is None:
             print("Linked List Underflow!!")
         else:
-            while current.get_next() is not None:
+            while current.next is not None:
                 previous = current
-                current = current.get_next()
+                current = current.next
 
             if previous is None:
                 self.head = None
             else:
-                previous.set_next(None)
-        self.print_data()
+                previous.next
+                None
 
+    # function to delete from the beginning of the list
     def delete_from_beg(self):
         current = self.head
 
         if current is None:
             print("Linked List Underflow!!")
         else:
-            self.head = current.get_next()
-        self.print_data()
+            self.head = current.next
 
-    def is_empty(self):
-        return self.head is None
-
+    # function to print the linked list data
     def print_data(self):
         current = self.head
 
         while current is not None:
-            print(current.get_data(), '->', end='')
-            current = current.get_next()
+            print(current.data, '->', end='')
+            current = current.next
         print('End of list')
 
+
+# Main program:
 if __name__ == '__main__':
+    # Create a singly linked list object
     linked_list = SinglyLinkedList()
+
+    # Insert at the beginning 3, 2, 1
     linked_list.insert_at_beg(3)
     linked_list.insert_at_beg(2)
     linked_list.insert_at_beg(1)
+    print('After insertion at the beginning:')
+    linked_list.print_data()
+
+    # Insert at the end of the list 4, 5
     linked_list.insert_at_end(4)
     linked_list.insert_at_end(5)
+    print('After insertion at the end:')
+    linked_list.print_data()
+
+    # Delete 1 from the beginning
+    print('After deletion at the beginning:')
     linked_list.delete_from_beg()
+    linked_list.print_data()
+
+    # Delete 5 from the end
+    print('After deletion at the end:')
     linked_list.delete_from_end()
+    linked_list.print_data()
+
+    # print size of the list
     print("size: ", linked_list.size())
